f2597f87b1425d640ecc94c01fee4cd7ec5b3096
anneroemer <77618343+anneroemer@users.noreply.github.com>
2025-03-07 16:36:33 +0100

    slug

--- app/artwork/[slug]/page.js
+++ app/artwork/[slug]/page.js
@@ -1,18 +1,21 @@
-"use client";
 import { Suspense } from "react";
 import Artwork from "../../components/Artwork";
+import path from "path";
 
 // export async function generateStaticParams() {
-//   const res = await fetch(`https://api.artic.edu/api/v1/artworks?limit=40`);
-//   const artworks = await res.json();
-//   console.log("Artworks:", artworks);
-//   return artworks.map((art) => ({
-//     slug: art.slug,
+//   const artworks = await fetch("https://api.artic.edu/api/v1/artworks").then((res) => res.json());
+//   console.log("Artworks:", artworks.data);
+//   if (!artworks.data || artworks.data.length === 0) {
+//     return { slug: "not-found" };
+//   }
+//   return artworks.data.map((artwork) => ({
+//     slug: `${artwork.id}`,
 //   }));
 // }
 
-export default function Page({ params }) {
-  //   const { slug } = await params;
+export default async function Page({ params }) {
+  // const { slug } = await params;
+
   return (
     <Suspense fallback={<div>loading</div>}>
       <Artwork params={params} />
--- app/page.js
+++ app/page.js
@@ -1,7 +1,38 @@
-import Artworks from "./components/Artworks";
+import { IoArrowForwardSharp } from "react-icons/io5";
+import styles from "./components/Artworks.module.scss";
+import Art from "./components/Art";
+import Link from "next/link";
 
-const Home = () => {
-  return <Artworks />;
-};
+async function getArtworks() {
+  const res = await fetch(`https://api.artic.edu/api/v1/artworks?limit=60`);
+  const artworks = await res.json();
+  console.log("art", artworks);
+  return artworks.data;
+}
 
-export default Home;
+export default async function Index() {
+  const artworks = await getArtworks();
+
+  return (
+    <ul className="list">
+      {artworks.map((artwork, index) => (
+        <li key={index} className={styles.listItem}>
+          <Link passHref={true} href={`/artwork/${artwork.id}`} className={styles.listItem__link} scroll>
+            <span className={styles.listItem__linkAnchor}>
+              <div className={styles.listItem__textBox}>
+                <h2 className={styles.listItem__title}>{artwork.title}</h2>
+                <p className={styles.listItem__artist}>{artwork.artist_title ? artwork.artist_title : "unknown"}</p>
+                <div className={styles.listItem__underline}></div>
+              </div>
+              <IoArrowForwardSharp className={styles.listItem__arrow} />
+              <div className={styles.listItem__imgContainer}>
+                <Art props={{ title: artwork.artist_title, id: artwork?.image_id }} />
+              </div>
+              <div className={styles.overlay}></div>
+            </span>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
