e632e2288ded0c5fedef929bf6d74bf704d4f633
anneroemer <77618343+anneroemer@users.noreply.github.com>
2025-03-07 17:20:25 +0100

    static params

--- app/artwork/[slug]/page.js
+++ app/artwork/[slug]/page.js
@@ -2,16 +2,16 @@ import { Suspense } from "react";
 import Artwork from "../../components/Artwork";
 import path from "path";
 
-// export async function generateStaticParams() {
-//   const artworks = await fetch("https://api.artic.edu/api/v1/artworks").then((res) => res.json());
-//   console.log("Artworks:", artworks.data);
-//   if (!artworks.data || artworks.data.length === 0) {
-//     return { slug: "not-found" };
-//   }
-//   return artworks.data.map((artwork) => ({
-//     slug: `${artwork.id}`,
-//   }));
-// }
+export async function generateStaticParams() {
+  const artworks = await fetch("https://api.artic.edu/api/v1/artworks").then((res) => res.json());
+  console.log("Artworks:", artworks.data);
+  if (!artworks.data || artworks.data.length === 0) {
+    return { slug: "not-found" };
+  }
+  return artworks.data.map((artwork) => ({
+    slug: `${artwork.id}`,
+  }));
+}
 
 export default async function Page({ params }) {
   // const { slug } = await params;
